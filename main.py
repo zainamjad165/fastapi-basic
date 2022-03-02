6f69104f794db5c84b084042544c695b5bf7f012
zainamjad165 <zainamjadbasra@gmail.com>
2022-03-02 16:00:37 +0500

    Mounting a FastAPI application

--- main.py
+++ main.py
@@ -1,35 +1,60 @@
 #Importing the FastApi class
 from fastapi import FastAPI, Response, status
 from fastapi.responses import JSONResponse
-# Creating an app object
-app = FastAPI()
 
-# Default route
+# Creating an app object
+homeapp = FastAPI()
+officeapp = FastAPI()
+
 
 # A minimal app to demonstrate the get request 
-@app.get("/", tags=['root'])
+@homeapp.get("/", tags=['root'])
+async def root() -> dict:
+    return {"Ping": "Pong"}
+
+@officeapp.get("/", tags=['root'])
 async def root() -> dict:
     return {"Ping": "Pong"}
 
 
 # GET -- > Read Todo 
-@app.get("/todo", tags=['Todos'])
+@homeapp.get("/todo", tags=['Home Todos'])
 async def get_todos(response: Response):
     response.headers["hello"] = "here are your todos."
-    return {"Data": todos}
+    return {"Data": hometodos}
 
+@officeapp.get("/todo", tags=['Todos'])
+async def get_todos(response: Response):
+    response.headers["hello"] = "here are your todos."
+    return {"Data": officetodos}
 
 # Post -- > Create Todo
-@app.post("/todo", tags=["Todos"])
+@homeapp.post("/todo", tags=["Home Todos"])
 async def add_todo(todo: dict) -> dict:
-    todos.append(todo)
+    hometodos.append(todo)
     return JSONResponse(status_code=201, content={"data": "A Todo is Added in Todos!"})
 
+@officeapp.post("/todo", tags=["Todos"])
+async def add_todo(todo: dict) -> dict:
+    officetodos.append(todo)
+    return JSONResponse(status_code=201, content={"data": "A Todo is Added in Todos!"})
 
 # PUT  -- > Update Todo
-@app.put("/todo/{id}",status_code=200, tags=["Todos"])
+@homeapp.put("/todo/{id}",status_code=200, tags=["Home Todos"])
 async def update_todo(id: int, body: dict, response: Response) -> dict:
-    for todo in todos:
+    for todo in hometodos:
+        if (int(todo["id"])) == id:
+            todo["Activity"] = body["Activity"]
+            return {
+                "data": f"Todo with id {id} has been updated"
+            }
+        else:
+            response.status_code = status.HTTP_201_CREATED
+            return {"data": f"The Todo with id {id} is not found!"}
+
+@officeapp.put("/todo/{id}",status_code=200, tags=["Todos"])
+async def update_todo(id: int, body: dict, response: Response) -> dict:
+    for todo in officetodos:
         if (int(todo["id"])) == id:
             todo["Activity"] = body["Activity"]
             return {
@@ -40,11 +65,11 @@ async def update_todo(id: int, body: dict, response: Response) -> dict:
             return {"data": f"The Todo with id {id} is not found!"}
 
 # DELETE --> Delete Todo 
-@app.delete("/todo/{id}", tags=["Todos"])
+@homeapp.delete("/todo/{id}", tags=["Home Todos"])
 async def delete_todo(id: int) -> dict:
-    for todo in todos:
+    for todo in hometodos:
         if int(todo["id"]) == id:
-            todos.remove(todo)
+            hometodos.remove(todo)
             return{
                 "data": f"Todo with id {id} has been deleted!"
             }
@@ -52,9 +77,21 @@ async def delete_todo(id: int) -> dict:
         "data": f"Todo with id {id} was not found!"
     }
 
-# Todos List
+@officeapp.delete("/todo/{id}", tags=["Todos"])
+async def delete_todo(id: int) -> dict:
+    for todo in officetodos:
+        if int(todo["id"]) == id:
+            officetodos.remove(todo)
+            return{
+                "data": f"Todo with id {id} has been deleted!"
+            }
+    return {
+        "data": f"Todo with id {id} was not found!"
+    }
 
-todos = [
+# Home Todos List
+
+hometodos = [
     {
         "id": "1",
         "Activity": "Jogging for 2 hours at 7:00 AM."
@@ -64,3 +101,19 @@ todos = [
         "Activity": "Writing 3 pages of my new book at 2:00 PM."
     }
 ]
+
+#Office Todos List
+
+officetodos = [
+    {
+        "id":"1",
+        "Activity": "Read the docomantation."
+    },
+    {
+        "id":"2",
+        "Activity": "Code the logic."
+    }
+]
+
+
+homeapp.mount("/subapi", officeapp)
